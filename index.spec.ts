a29552c06755e57538fade37dfc3deccd0af4d25
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-03-02 03:54:11 +0800

    feat: add helper function to convert html to pdf

--- index.spec.ts
+++ index.spec.ts
@@ -1,8 +1,26 @@
 import { expect } from 'chai'
-import { is_soffice_installed } from './index'
+import { convert_to_pdf, is_soffice_installed } from './index'
+import { existsSync, unlinkSync, writeFileSync } from 'fs'
 
 describe('check if soffice is installed', () => {
   it('should return true if LibreOffice is installed', () => {
     expect(is_soffice_installed()).to.be.true
+  })
+})
+
+describe('pdf conversion', () => {
+  before(async () => {
+    let res = await fetch('https://www.example.com')
+    let text = await res.text()
+    writeFileSync('res/test.html', text)
+  })
+  it('should convert html to pdf', () => {
+    let input_file = 'res/test.html'
+    let output_file = 'res/test.pdf'
+    if (existsSync(output_file)) {
+      unlinkSync(output_file)
+    }
+    expect(convert_to_pdf(input_file)).to.equals(output_file)
+    expect(existsSync(output_file)).to.be.true
   })
 })
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { execSync } from 'child_process'
+import { dirname, extname } from 'path'
 
 export function is_soffice_installed() {
   try {
@@ -8,4 +9,14 @@ export function is_soffice_installed() {
     // e.g. not in PATH
     return false
   }
+}
+
+export function convert_to_pdf(input_file: string) {
+  let ext = extname(input_file)
+  let output_file = input_file.slice(0, input_file.length - ext.length) + '.pdf'
+  let dir = dirname(input_file)
+  execSync(
+    `soffice --headless --convert-to pdf:writer_pdf_Export ${input_file} --outdir ${dir}`,
+  )
+  return output_file
 }
